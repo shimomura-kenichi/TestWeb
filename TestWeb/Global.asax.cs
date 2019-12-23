5ec4e249ec3462195a504037c0acbe6b43a3428d
nakadate <nakadateabc@gmail.com>
2019-12-23 21:50:56 +0900

    dddddd

---
+++ TestWeb/Global.asax.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace TestWeb
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ TestWeb/Models/Login/ViewModel/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace TestWeb.Models.Login.ViewModel
+{
+    public class LoginViewModel
+    {
+        [DisplayName("ユーザーID")]
+        public string UserId { get; set; }
+        [DisplayName("パスワード")]
+        public string Password { get; set; }
+        [DisplayName("お知らせ")]
+        public string Infomation { get; set; }
+    }
+}
